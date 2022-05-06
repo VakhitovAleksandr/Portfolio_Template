a49411c17be44b5dfd8a135026df0bc9064ca55f
Vakhitov Aleksandr <ale.vakhitov@gmail.com>
2022-05-06 23:00:49 +0500

    06.05.22

---
+++ js/modules/tabs.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function Tabs(tabNavsSelector, tabBodySelector, tabLinksSelector) {
+  this.nav = document.querySelector(tabNavsSelector);
+  this.body = document.querySelectorAll(tabBodySelector);
+  this.links = document.querySelectorAll(tabLinksSelector);
+  this.hide = function () {
+    this.body.forEach((item) => {
+      item.style.display = "none";
+      this.links.forEach((item) => {
+        item.classList.remove("active");
+      });
+    });
+  };
+  this.show = function (i = 0) {
+    this.body[i].style.display = "block";
+    this.links[i].classList.add("active");
+  };
+  this.hide();
+  this.show();
+  this.nav.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (e.target.classList.contains(tabLinksSelector.replace(/\./, ""))) {
+      this.links.forEach((item, i) => {
+        if (e.target == item) {
+          this.hide();
+          this.show(i);
+        }
+      });
+    }
+  });
+}
+
+export default Tabs;
---
+++ js/script.js
@@ -0,0 +1,5 @@
+import Tabs from "./modules/tabs.js";
+
+window.addEventListener('DOMContentLoaded', function(){
+    const tab = new Tabs('.tabs__nav','.tabs__item', '.tabs__link');
+});
